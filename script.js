cee8d8e27eb636b0ece253b853511b9adb64491c
DaniilRDO <daniil5679888@gmail.com>
2026-09-11 10:18:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let gameState = {
     clickUpgradeLevel: 0,
     
     // Батрачки (пасивний дохід)
-    workers: 0,
+    workers: 1,
     
     // Фінгер смерті
     fingerPurchasedCount: 0, // Скільки разів купляли
